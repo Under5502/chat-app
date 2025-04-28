5ba2134c5b33beea622fa7ac0e6f8a0a776eb864
Nguyen Van A <nguyenvana@gmail.com>
2025-04-28 23:02:49 +0700

    cm

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { createServer } from "http"; // Đảm bảo sử dụng createServer từ http
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { initSocket } from "./lib/socket.js"; // Import chỉ initSocket từ socket.js
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
-const server = createServer(app); // Tạo server với app express
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
@@ -25,11 +25,9 @@ app.use(
   })
 );
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Static files for production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -38,12 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Khởi tạo socket sau khi tạo server
-initSocket(server);
-
-const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
